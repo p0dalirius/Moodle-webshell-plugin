0bbf9d04d38159a4830fc7d8a49b31fbd9f008c0
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2025-02-11 09:03:15 +0100

    Updated banner

--- moodle_webshell/lang/en/local_moodle_webshell.php
+++ moodle_webshell/lang/en/local_moodle_webshell.php
@@ -3,10 +3,11 @@
 /**
  * @package  moodle_webshell
  * @copyright 2022, Remi GASCOU (Podalirius) <podalirius@protonmail.com>
+ * @url https://github.com/p0dalirius/Moodle-webshell-plugin
  * @license MIT
  * @doc https://docs.moodle.org/dev/String_API
  */
  
 defined('MOODLE_INTERNAL') || die();
  
-$string['plugintitle'] = 'Moodle webshell plugin - by Podalirius';
+$string['plugintitle'] = 'Moodle webshell plugin - by Remi GASCOU (Podalirius)';
